a8bf988b57da3071e2f1e60c9e34a2e5195f0660
quiettone <quiettonez@gmail.com>
2026-06-21 16:00:54 +0300

    Fix hunger/fire freeze in Fabric, freeze new mobs on Paper, preserve Water Breathing

--- fabric/src/main/java/com/quiettone/quietpause/mixin/FireBlockMixin.java
+++ fabric/src/main/java/com/quiettone/quietpause/mixin/FireBlockMixin.java
@@ -2,8 +2,9 @@ package com.quiettone.quietpause.mixin;
 
 import com.quiettone.quietpause.PauseManager;
 import com.quiettone.quietpause.QuietPause;
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.AbstractFireBlock;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.FireBlock;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.random.Random;
@@ -12,10 +13,11 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(FireBlock.class)
+@Mixin(AbstractBlock.class)
 public class FireBlockMixin {
     @Inject(method = "randomTick", at = @At("HEAD"), cancellable = true)
     private void quietpause$cancelFireSpread(BlockState state, ServerWorld world, BlockPos pos, Random random, CallbackInfo ci) {
+        if (!(((Object) this) instanceof AbstractFireBlock)) return;
         PauseManager manager = QuietPause.getPauseManager();
         if (manager != null && manager.isFrozen()) {
             ci.cancel();
